111050ae9a31d8a259ed1fa7fd8a4e565a98e6ef
abidmuin <21988951+abidmuin@users.noreply.github.com>
2026-04-21 01:01:21 -0300

    chore(nvim): update which-key

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -3,31 +3,14 @@
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
-
-	config = function()
-		local wk = require("which-key")
-
-		vim.o.timeout = true
-		vim.o.timeoutlen = 500
-
-		wk.setup({
-			preset = "modern",
-			delay = 500,
-
-			icons = {
-				mappings = true,
-				keys = true,
-			},
-		})
-
-		wk.add({
-			{ "<leader>f", group = "Find / Telescope" },
-			{ "<leader>h", group = "Git Hunks" },
-			{ "<leader>m", group = "Format" },
-
-			{ "]", group = "Next" },
-			{ "[", group = "Previous" },
-			{ "g", group = "Go To" },
-		})
-	end,
+	opts = {},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
