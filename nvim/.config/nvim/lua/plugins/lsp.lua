cdc0fcd4008dde100409774b5a5c2d5b4b3df782
abidmuin <21988951+abidmuin@users.noreply.github.com>
2026-04-21 01:11:12 -0300

    style(nvim): change keybind for diagnostic float message

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -44,7 +44,7 @@ return {
 			float = { border = "rounded" },
 		})
 
-		vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+		vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
 		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 		vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 
