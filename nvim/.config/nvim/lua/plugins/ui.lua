e01da23b290826ecfc59042fda29c35bab608fdc
abidmuin <21988951+abidmuin@users.noreply.github.com>
2026-04-21 01:05:34 -0300

    chore(nvim): remove comments

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,8 +1,9 @@
+-- ~/.config/nvim/lua/plugins/ui.lua
+
 return {
 	"folke/snacks.nvim",
 	priority = 1000,
 	lazy = false,
-	---@type snacks.Config
 	opts = {
 		bigfile = { enabled = true },
 		dashboard = { enabled = true },
@@ -559,7 +560,6 @@ return {
 		vim.api.nvim_create_autocmd("User", {
 			pattern = "VeryLazy",
 			callback = function()
-				-- Setup some globals for debugging (lazy-loaded)
 				_G.dd = function(...)
 					Snacks.debug.inspect(...)
 				end
@@ -567,7 +567,6 @@ return {
 					Snacks.debug.backtrace()
 				end
 
-				-- Override print to use snacks for `:=` command
 				if vim.fn.has("nvim-0.11") == 1 then
 					vim._print = function(_, ...)
 						dd(...)
@@ -576,7 +575,6 @@ return {
 					vim.print = _G.dd
 				end
 
-				-- Create some toggle mappings
 				Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
 				Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
 				Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
