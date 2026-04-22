2969fb45c61816ce5b362e4cbab972c15d4af239
abidmuin <21988951+abidmuin@users.noreply.github.com>
2026-04-22 12:32:25 -0300

    feat(nvim): add markdown support

---
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,9 @@
+-- ~/.config/nvim/lua/plugins/markdown.lua
+
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	-- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
+	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+	opts = {},
+}
